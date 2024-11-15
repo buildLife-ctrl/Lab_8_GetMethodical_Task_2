694af26579e9b8bb53fd626c8ba934a467aa080a
617998 <sinchuri.617998@swl.k12.oh.us>
2024-11-15 08:36:44 -0500

    2ini

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class InputHelper {
+    public static int getInt(Scanner scan, String prompt) {
+        int num = 0; //eventually returned to user after validated
+        boolean check = false;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                num = scan.nextInt();
+                check = true;
+            } else {
+                System.out.println("Invalid input, please try again");
+            }
+            scan.nextLine();
+        } while (!check);
+
+        return num;
+    }
+
+    public static double getDouble(Scanner scan, String prompt) {
+        double input = 0;
+        boolean check = false;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextDouble()) {
+                input = scan.nextDouble();
+                if (input % 1 != 0) {
+                    check = true;
+                } else {
+                    System.out.println("Please enter a decimal value.");
+                }
+            } else {
+                System.out.println("Invalid input, please try again");
+            }
+            scan.nextLine();
+        } while (!check);
+
+        return input;
+    }
 }
--- src/TaskTwoFavNumbers.java
+++ src/TaskTwoFavNumbers.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TaskTwoFavNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int favInt = InputHelper.getInt(scan, "What is your favorite integer?");
+        double favDouble = InputHelper.getDouble(scan, "What is your favorite double?");
+        System.out.println("Your favorite integer is " + favInt + " and your favorite double is " + favDouble);
+    }
 }
